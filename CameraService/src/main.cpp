b263628cb4926c6d7e306f46fe46697a97ecf1f7
sfoxover <sfoxover@gmail.com>
2019-12-22 17:19:21 -0800

    - add new command server binding to control settings

--- CameraService/src/main.cpp
+++ CameraService/src/main.cpp
@@ -11,28 +11,60 @@
 #endif
 #include "Settings.h"
 #include "DetectFaces.h"
+#include <CommandServer.h>
 
 int main()
 {
+	// Load settings from json file
 	bool bOK = true;
 	std::wstring error;
-    std::string path = Helpers::AppendToRunPath("message_developer.json");
+	std::string path = Helpers::AppendToRunPath("message_developer.json");
 	bOK = CSettings::Instance().Initialize(path, error);
-	if(!bOK)
+	assert(bOK);
+	if (!bOK)
 	{
 		std::wcout << L"Camera Service settings initialize failed. Error, " << error << std::endl;
 	}
-	assert(bOK);
-
-	// Initialize facial detection
-	if(CSettings::Instance().GetUseFaceDetect())
+	else
 	{
-		CDetectFaces::Instance().Initialize(CSettings::Instance().GetFaceDetectMethod());
-	}
+		// Initialize facial detection
+		if (CSettings::Instance().GetUseFaceDetect())
+		{
+			CDetectFaces::Instance().Initialize(CSettings::Instance().GetFaceDetectMethod());
+		}
 
-	// Set up publisher endpoints
-	bOK = CPublishMessage::Instance().Initialize(CSettings::Instance().GetPublishUris(), error);
-	assert(bOK);
+		// Set up publisher endpoints
+		bOK = CPublishMessage::Instance().Initialize(CSettings::Instance().GetPublishUris(), error);
+		assert(bOK);
+
+		// Set up command server endpoints
+		if (bOK)
+		{
+			bOK = CommandServer::Instance().Start(error);
+			assert(bOK);
+
+			// Load sample video
+			if (bOK)
+			{
+				if (CSettings::Instance().GetUseSampleVideo())
+				{
+					auto filePath = Helpers::AppendToRunPath(Helpers::AppendPath("assets", CSettings::Instance().GetSampleVideoName()));
+					bool exists = Helpers::FileExists(filePath);
+					assert(exists);
+
+					// Publish video stream
+					bOK = CVideoSource::Instance().Start(Helpers::Utf8ToWide(filePath), error);
+					assert(bOK);
+				}
+				else
+				{
+					// Publish camera stream
+					bOK = CVideoSource::Instance().Start(error);
+					assert(bOK);
+				}
+			}
+		}
+	}
 	if (bOK)
 	{
 		std::wcout << L"Camera Service initialized OK" << std::endl;
@@ -41,27 +73,6 @@ int main()
 	{
 		std::wcout << L"Camera Service initialize failed. Error, " << error << std::endl;
 	}
-
-	// Load sample video
-	if (CSettings::Instance().GetUseSampleVideo())
-	{
-		auto filePath = Helpers::AppendToRunPath(Helpers::AppendPath("assets", CSettings::Instance().GetSampleVideoName()));
-		bool exists = Helpers::FileExists(filePath);
-		assert(exists);
-
-		// Publish video stream
-		bOK = CVideoSource::Instance().Start(Helpers::Utf8ToWide(filePath), error);
-		assert(bOK);
-	}
-	else
-	{
-		// Publish camera stream
-		bOK = CVideoSource::Instance().Start(error);
-		assert(bOK);
-	}
-
-	// Start motion sensor on GPIO 7 pin 26
-//	CMotionSensor::Instance().Start(7, error);
 
 	// Wait for enter
 	std::cin.get();
